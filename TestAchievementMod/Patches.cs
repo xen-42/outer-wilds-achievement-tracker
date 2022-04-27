c8f405552de2a6b3f0cfdbb0f89207e70ef75ab6
Nick <nicholasconnors@hotmail.ca>
2022-04-26 21:38:00 -0400

    Add more to the test mod

---
+++ TestAchievementMod/Patches.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAchievementMod
+{
+    public static class Patches
+    {
+        public static void Apply()
+        {
+            TestAchievementMod.Instance.ModHelper.HarmonyHelper.AddPrefix<AudioSignal>(nameof(AudioSignal.IdentifySignal), typeof(Patches), nameof(Patches.IdentifySignal));
+            TestAchievementMod.Instance.ModHelper.HarmonyHelper.AddPrefix<CharacterDialogueTree>(nameof(CharacterDialogueTree.EndConversation), typeof(Patches), nameof(Patches.EndConversation));
+        }
+
+        public static void IdentifySignal()
+        {
+            TestAchievementMod.Instance.AchievementsAPI.EarnAchievement("TEST.SIGNAL");
+        }
+
+        public static void EndConversation()
+        {
+            TestAchievementMod.Instance.AchievementsAPI.EarnAchievement("TEST.DIALOGUE");
+        }
+    }
+}
--- TestAchievementMod/TestAchievementMod.cs
+++ TestAchievementMod/TestAchievementMod.cs
@@ -8,10 +8,14 @@ namespace TestAchievementMod
 {
     public class TestAchievementMod : ModBehaviour
     {
+        public static TestAchievementMod Instance;
+
         public IAchievements AchievementsAPI;
 
         private void Start()
         {
+            Instance = this;
+
             SceneManager.sceneLoaded += OnSceneLoaded;
 
             AchievementsAPI = ModHelper.Interaction.GetModApi<IAchievements>("xen.AchievementTracker");
@@ -24,19 +28,33 @@ namespace TestAchievementMod
 
             // This one will use the GlobalMessenger
             AchievementsAPI.RegisterAchievement("TEST.MARSHMALLOW", false, this);
+            AchievementsAPI.RegisterAchievement("TEST.DIE", true, this);
 
             // Earn an achievement when you finish roasting a marshmallow
-            GlobalMessenger.AddListener("ExitRoastingMode", new Callback(OnExitRoastingMode));
+            GlobalMessenger.AddListener("ExitRoastingMode", OnExitRoastingMode);
+            GlobalMessenger<DeathType>.AddListener("PlayerDeath", OnPlayerDeath);
+
+            // These ones will use patches
+            AchievementsAPI.RegisterAchievement("TEST.SIGNAL", false, this);
+            AchievementsAPI.RegisterAchievement("TEST.DIALOGUE", false, this);
+
+            Patches.Apply();
 
             // Now add the translation stuff
             AchievementsAPI.RegisterTranslation("TEST.SOLAR_SYSTEM", TextTranslation.Language.ENGLISH, "Start", "You started the game.");
             AchievementsAPI.RegisterTranslation("TEST.EYE", TextTranslation.Language.ENGLISH, "End", "You ended the game.");
+            
             AchievementsAPI.RegisterTranslation("TEST.MARSHMALLOW", TextTranslation.Language.ENGLISH, "Marshmallow", "Roast a marshmallow. Better than NH.");
+            AchievementsAPI.RegisterTranslation("TEST.DIE", TextTranslation.Language.ENGLISH, "Ow", "Die.");
+
+            AchievementsAPI.RegisterTranslation("TEST.SIGNAL", TextTranslation.Language.ENGLISH, "Signals++", "Identify a signal.");
+            AchievementsAPI.RegisterTranslation("TEST.DIALOGUE", TextTranslation.Language.ENGLISH, "Fait beau eh?", "Talk to somebody.");
         }
 
         private void OnDestroy()
         {
-            GlobalMessenger.RemoveListener("ExitRoastingMode", new Callback(OnExitRoastingMode));
+            GlobalMessenger.RemoveListener("ExitRoastingMode", OnExitRoastingMode);
+            GlobalMessenger<DeathType>.RemoveListener("PlayerDeath", OnPlayerDeath);
         }
 
         private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
@@ -57,6 +75,11 @@ namespace TestAchievementMod
         private void OnExitRoastingMode()
         {
             AchievementsAPI.EarnAchievement("TEST.MARSHMALLOW");
+        }
+
+        private void OnPlayerDeath(DeathType _)
+        {
+            AchievementsAPI.EarnAchievement("TEST.DIE");
         }
     }
 }
