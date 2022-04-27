d527d080163ae7caae14038fbc9b74ead241f948
Nick <nicholasconnors@hotmail.ca>
2022-04-27 00:35:25 -0400

    Update IAchievements.cs

--- TestAchievementMod/IAchievements.cs
+++ TestAchievementMod/IAchievements.cs
@@ -11,6 +11,7 @@ namespace TestAchievementMod
     {
         void RegisterAchievement(string uniqueID, bool secret, ModBehaviour mod);
         void RegisterTranslation(string uniqueID, TextTranslation.Language language, string name, string description);
+        void RegisterTranslationsFromFiles(ModBehaviour mod, string folderPath);
         void EarnAchievement(string uniqueID);
     }
 }
